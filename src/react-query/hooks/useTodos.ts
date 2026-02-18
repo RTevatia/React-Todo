1a69a5d7bfa29a0d0c975f426654c8a0a4195dda
Rahul Tevatia <rtevatia2@gmail.com>
2026-02-18 06:58:05 -0500

    Refactor: Create todoService  - a resuable HTTP service
    Create shared todoService and centralize Todo type to eliminate endpoint duplication

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,21 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
 import { CACHE_KEY_TODOS } from "../constants";
+import todoService, { Todo } from "../services/todo-service";
 
-const apiClient = new APIClient<Todo>("/todos");
 
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
 
 const useTodos = () => {
 
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: apiClient.getall,
+    queryFn: todoService.getall,
     staleTime: 10 * 1000, // 10s
   });
 };
