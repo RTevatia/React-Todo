354914108cf11c18f0c546e0afd481a144268edc
Rahul Tevatia <rtevatia2@gmail.com>
2026-03-09 07:33:19 -0400

    refactor: manage current user state using Zustand store

--- src/state-management/tasks/TaskList.tsx
+++ src/state-management/tasks/TaskList.tsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import useAuth from "../auth/useAuth";
 import TasksContext from "./tasksContext";
+import useAuthStore from "../auth/store";
 
 const TaskList = () => {
   const useTasks = () => useContext(TasksContext);
 
   const { tasks, dispatch } = useTasks();
-  const { user } = useAuth();
+  const { user } = useAuthStore();
 
   return (
     <>
